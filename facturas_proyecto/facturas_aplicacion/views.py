34fbb7d58e74732e360b0949f9a43b465a24c8b2
DYusu <abadilla2009@gmail.com>
2024-07-29 23:41:00 -0400

    creado_index_basico

--- facturas_proyecto/facturas_aplicacion/views.py
+++ facturas_proyecto/facturas_aplicacion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Factura, Empresa, Receptor
 
-# Create your views here.
+def index(request):
+    facturas = Factura.objects.all()
+    empresas = Empresa.objects.all()
+    receptores = Receptor.objects.all()
+
+    context = {
+        'facturas': facturas,
+        'empresas': empresas,
+        'receptores': receptores,
+    }
+    return render(request, 'index.html', context)
